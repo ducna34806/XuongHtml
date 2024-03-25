40dcf0a34b6f402e4e9e843bf3975b722cac921d
ducnaph <ducnaph34806@fpt.edu.vn>
2024-03-26 02:47:05 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        1016: "1016px",
+        608: "608px",
+        453: "453px",
+        527: "527px",
+        76: "76px",
+        481: "481px",
+      },
+      height: {
+        500: "500px",
+      },
+    },
+  },
+  plugins: [],
+};
